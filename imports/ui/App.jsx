171ca3ee774585ddd08e83ab779923437f9905b4
samuelahmed <samueladuval@gmail.com>
2023-09-16 11:42:33 -0700

    remove unused import

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Toolbar } from "./layouts/Toolbar";
 import { Flowchart } from "./layouts/Flowchart";
-import { Button } from "./components/ui/button";
 
 export const App = () => (
   <main className="flex min-h-screen">
+
     {/* Left Menu */}
     <div className="w-fit">
       <Toolbar />
     </div>
+
     {/* Main Content */}
     <div className="w-3/4">
       <Flowchart />
     </div>
+    
   </main>
 );
