f50dff96e959a17e94d018b54b3e6daec15d27e4
samuelahmed <samueladuval@gmail.com>
2023-09-16 18:32:36 -0700

    add tooltips to remind users to drag into flow area

--- imports/ui/layouts/FlowchartControls.jsx
+++ imports/ui/layouts/FlowchartControls.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import { Button } from "../components/ui/button";
 import { Switch } from "../components/ui/switch";
 import { Separator } from "../components/ui/seperator";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../components/ui/tooltip";
 
 export const FlowchartControls = () => {
   const onDragStart = (event, nodeType) => {
@@ -12,48 +18,88 @@ export const FlowchartControls = () => {
   return (
     <>
       <h2 className="text-muted-foreground text-center">Workflows</h2>
+
       <Separator />
       {/* Create Unit */}
       <p className="text-sm text-muted-foreground">Create Unit</p>
+
       <div className="flex flex-col md:flex-row justify-left md:space-x-2 space-y-2 md:space-y-0">
-        <Button
-          variant="outline"
-          size="sm"
-          className="outline outline-blue-500 w-20"
-          onDragStart={(event) => onDragStart(event, "Execution")}
-          draggable
-        >
-          Execution
-        </Button>
-        <Button
-          variant="outline"
-          size="sm"
-          className="outline outline-green-500 w-20"
-          onDragStart={(event) => onDragStart(event, "Condition")}
-          draggable
-        >
-          Condition
-        </Button>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Button
+                variant="outline"
+                size="sm"
+                className="outline outline-blue-500 w-20"
+                onDragStart={(event) => onDragStart(event, "Execution")}
+                draggable
+              >
+                Execution
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Drag into dotted area to create Execution Unit</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Button
+                variant="outline"
+                size="sm"
+                className="outline outline-green-500 w-20"
+                onDragStart={(event) => onDragStart(event, "Condition")}
+                draggable
+              >
+                Condition
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Drag into dotted area to create Condition Unit</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </div>
       <div className="flex flex-col md:flex-row justify-left md:space-x-2 space-y-2 md:space-y-0">
-        <Button
-          variant="outline"
-          size="sm"
-          className="outline outline-yellow-500 w-20"
-          onDragStart={(event) => onDragStart(event, "Assignment")}
-          draggable
-        >
-          Assignment
-        </Button>
-        <Button
-          variant="outline"
-          size="sm"
-          className="outline outline-red-500 w-20"
-          onDragStart={(event) => onDragStart(event, "Assertion")}
-          draggable
-        >
-          Assertion
-        </Button>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Button
+                variant="outline"
+                size="sm"
+                className="outline outline-yellow-500 w-20"
+                onDragStart={(event) => onDragStart(event, "Assignment")}
+                draggable
+              >
+                Assignment
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Drag into dotted area to create Assignment Unit</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Button
+                variant="outline"
+                size="sm"
+                className="outline outline-red-500 w-20"
+                onDragStart={(event) => onDragStart(event, "Assertion")}
+                draggable
+              >
+                Assertion
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Drag into dotted area to create Assertion Unit</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </div>
       <Separator />
       <div className="md:flex justify-start space-x-2">
